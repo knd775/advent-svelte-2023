8c7d05dddf4d49d5ab06fd6406c1a038931218e2
Ben Ayles <knd775@users.noreply.github.com>
2023-12-30 14:14:04 -0500

    Improve mobile ux

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px'
+      padding: {
+        DEFAULT: '1rem',
+        md: '2rem'
       }
     },
     extend: {
@@ -56,6 +56,9 @@ const config = {
       },
       fontFamily: {
         sans: [...fontFamily.sans]
+      },
+      screens: {
+        '2xl': '1400px'
       }
     }
   }
