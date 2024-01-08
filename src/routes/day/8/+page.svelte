2abe232d389ba90456738c186f26d9cc8df2023d
Ben Ayles <knd775@users.noreply.github.com>
2024-01-07 23:30:48 -0500

    Day 8, mostly done

--- src/routes/day/8/+page.svelte
+++ src/routes/day/8/+page.svelte
@@ -7,26 +7,73 @@
 </script>
 
 <Card.Root>
-  <Card.Header class="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+  <Card.Header class="grid grid-cols-[1fr_150px] items-start gap-4 space-y-0">
     <div class="space-y-1">
       <Card.Title>Deck of Doubles</Card.Title>
-      <Card.Description>Try to match all 24 pairs</Card.Description>
+      <Card.Description>
+        <p>Try to match all 24 pairs</p>
+        <p class="italic">Currently dealing with a likely svelte 5 reactivity bug</p>
+      </Card.Description>
     </div>
-    <div class="flex items-center justify-end space-x-1 rounded-md">
+    <div class="flex items-center justify-end space-x-4 rounded-md">
+      {#if game.active}
+        <p class="text-lg">{game.timer}</p>
+      {/if}
       <Button on:click={() => game.reset()}>Reset</Button>
     </div>
   </Card.Header>
   <Card.Content class="grid grid-cols-6 gap-1 md:grid-cols-12">
     {#each game.cards as card}
-      {#if card.flipped}
-        <img src={card.imageUrl} alt="card number {card.value}" />
-      {:else}
+      <div class="flex">
+        <img
+          src={card.imageUrl}
+          alt="card number {card.value}"
+          class="!aspect-[2/3]"
+          class:hidden={!card.flipped}
+          class:saturate-50={card.matched}
+        />
         <button
-          class="!aspect-[2/3] w-full rounded-sm bg-red-900"
+          class="!aspect-[2/3] w-full rounded-sm bg-[url('/cardback.webp')] bg-contain bg-no-repeat"
+          class:hidden={card.flipped}
           onclick={() => card.flip()}
           on:keydown
-        ></button>
-      {/if}
+        />
+        <div class="absolute font-bold text-green-400">
+          <p>f: {card.flipped}</p>
+          <p>m: {card.matched}</p>
+          <p>v: {card.value}</p>
+        </div>
+      </div>
     {/each}
   </Card.Content>
 </Card.Root>
+
+<Card.Root class="mt-4 w-44">
+  <Card.Header>
+    <Card.Title>Debug info</Card.Title>
+    <Card.Description>
+      <p>blocked: {game.blocked}</p>
+      <p>num flipped: {game.flippedCards.length}</p>
+      <p>num matched: {game.numberOfMatches}</p>
+    </Card.Description>
+  </Card.Header>
+</Card.Root>
+
+<style>
+  @keyframes flip {
+    from {
+      transform: rotate3d(1, 0, 0, -180deg);
+      animation-timing-function: ease-out;
+    }
+
+    to {
+      transform: rotate3d(1, 0, 0, 0deg);
+      animation-timing-function: ease-in;
+    }
+  }
+
+  .flip {
+    backface-visibility: visible;
+    animation-name: flip;
+  }
+</style>
--- src/routes/day/8/game.svelte.ts
+++ src/routes/day/8/game.svelte.ts
@@ -1,13 +1,71 @@
-const cardImageUrlBase = 'https://advent.sveltesociety.dev/data/2023/day-eight/';
+const cardImageUrlBase = 'https://advent.sveltesociety.dev/data/2023/day-eight';
 
 export class CardGame {
-  cards: Card[] = [];
+  cards: Card[] = $state([]);
+  blocked = $derived(this.flippedCards.length >= 2);
+  active = $state(false);
+  timer = $state(0);
+  #interval: number | null = null;
+
   constructor() {
+    $effect(() => {
+      this.createDeck();
+      this.shuffle();
+    });
+
+    $effect(() => {
+      if (this.flippedCards.length === 2) {
+        const card1 = this.flippedCards[0]!;
+        const card2 = this.flippedCards[1]!;
+        if (card1.value === card2.value) {
+          card1.matched = true;
+          card2.matched = true;
+        } else {
+          console.log('no match', this.flippedCards);
+          this.flippedCards.forEach((card) => {
+            setTimeout(() => {
+              console.log('unflipping', card, card.flipped);
+              card.unflip();
+              console.log('unflipped', card, card.flipped);
+            }, 750);
+          });
+        }
+      }
+      return () => {
+        console.log('checking cards', this.flippedCards, this.flippedCards.length);
+      };
+    });
+
+    $effect(() => {
+      if (this.numberOfMatches === 24) {
+        this.active = false;
+      }
+    });
+
+    $effect(() => {
+      if (!this.active && this.flippedCards.length > 0) {
+        this.active = true;
+      }
+    });
+
+    $effect(() => {
+      if (this.active) {
+        this.#interval = setInterval(() => {
+          this.timer++;
+        }, 1000);
+      } else if (this.#interval) {
+        clearInterval(this.#interval);
+        this.timer = 0;
+      }
+    });
+  }
+
+  createDeck() {
+    this.cards = [];
     for (let i = 1; i <= 24; i++) {
-      this.cards.push(new Card(i));
-      this.cards.push(new Card(i));
+      this.cards.push(new Card(i, this));
+      this.cards.push(new Card(i, this));
     }
-    this.shuffle();
   }
 
   shuffle() {
@@ -19,18 +77,23 @@ export class CardGame {
       currentIndex--;
 
       [this.cards[currentIndex], this.cards[randomIndex]] = [
-        this.cards[randomIndex] ?? new Card(0),
-        this.cards[currentIndex] ?? new Card(0)
+        this.cards[randomIndex] ?? new Card(0, this),
+        this.cards[currentIndex] ?? new Card(0, this)
       ];
     }
   }
 
   reset() {
-    this.cards.forEach((card) => {
-      card.matched = false;
-      card.flipped = false;
-    });
+    this.createDeck();
     this.shuffle();
+  }
+
+  get numberOfMatches() {
+    return this.cards.filter((card) => card.matched).length / 2;
+  }
+
+  get flippedCards() {
+    return this.cards.filter((card) => card.flipped && !card.matched);
   }
 }
 
@@ -38,16 +101,23 @@ class Card {
   value: number;
   matched = $state(false);
   flipped = $state(false);
+  game: CardGame;
 
-  constructor(value: number) {
+  constructor(value: number, game: CardGame) {
     this.value = value;
+    this.game = game;
   }
 
   flip() {
-    this.flipped = !this.flipped;
+    if (this.game.blocked) return;
+    this.flipped = true;
+  }
+
+  unflip() {
+    this.flipped = false;
   }
 
   get imageUrl() {
-    return `${cardImageUrlBase}${this.value}.png`;
+    return `${cardImageUrlBase}/${this.value}.png`;
   }
 }
